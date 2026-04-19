887f5fd9671f60b99c870311578a2a07469281d9
johad24 <147829081+johad24@users.noreply.github.com>
2026-04-19 08:47:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    const menu = document.getElementById("menu");
+
+    if (window.scrollY > 50) {
+        menu.classList.add("scrolled");
+    } else {
+        menu.classList.remove("scrolled");
+    }
+});
